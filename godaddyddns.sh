99fe5a9e56d3434ae1164fe3e2c022813f853c62
Quentin McGaw <quentin.mcgaw@gmail.com>
2018-02-01 11:55:01 -0500

    Removed echo to save on RAM for container

--- godaddyddns.sh
+++ godaddyddns.sh
@@ -6,18 +6,18 @@ fi
 if [[ -z "${DELAY}" ]]; then
     DELAY=300
 fi
-while:
+while true;
 do
-    echo "Checking for IP address change..."
-    godaddyresult=$(curl -s -X GET -H "Authorization: sso-key $KEY:$SECRET" "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$NAME")
-    godaddyip=$(echo $godaddyresult | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
-    ipresult=$(curl -s GET https://diagnostic.opendns.com/myip)
-    ip=$(echo $ipresult | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b")
+    # echo "Checking for IP address change...";
+    godaddyresult=$(curl -s -X GET -H "Authorization: sso-key $KEY:$SECRET" "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$NAME");
+    godaddyip=$(echo $godaddyresult | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b");
+    ipresult=$(curl -s GET https://diagnostic.opendns.com/myip);
+    ip=$(echo $ipresult | grep -oE "\b([0-9]{1,3}\.){3}[0-9]{1,3}\b");
     if [ "$ip" != "$godaddyip" ];
      then
-        echo "Updating $DOMAIN to new IP address $ip ..."
-        curl -i -s -X PUT -H "Authorization: sso-key $KEY:$SECRET" -H "Content-Type: application/json" -d '{"data":"'$ip'","ttl":600}' "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$NAME"
+        # echo "Updating $DOMAIN to new IP address $ip ..."
+        result=$(curl -i -s -X PUT -H "Authorization: sso-key $KEY:$SECRET" -H "Content-Type: application/json" -d '{"data":"'$ip'","ttl":600}' "https://api.godaddy.com/v1/domains/$DOMAIN/records/A/$NAME")
     fi
-    echo "DONE. Waiting for $DELAY seconds."
-    sleep "$DELAY"
+    # echo "DONE. Waiting for $DELAY seconds.";
+    sleep "$DELAY";
 done
